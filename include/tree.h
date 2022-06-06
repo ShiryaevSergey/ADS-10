c918c0549aa1a5ec6b096422e9ed99270c8c1536
puzhx <99177486+ShiryaevSergey@users.noreply.github.com>
2022-06-07 00:11:43 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,24 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <cstdint>
+#include <string>
+#include <vector>
+
+class Tree {
+ private:
+    struct Node;
+    std::vector<std::string> repl;
+    Node* parent;
+
+    void createTree(Node* parent, std::vector<char> trail);
+
+    void perms(Node* parent, std::string symb);
+
+ public:
+    explicit Tree(const std::vector<char>);
+
+    std::string operator[] (unsigned int i) const;
+};
+
 #endif  // INCLUDE_TREE_H_
